b3ede3ffcf55b7521b3d316cad471aee3ee9308c
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2024-05-16 23:54:53 -0300

    chore: refining streams data

--- zoomdl/data/streams.py
+++ zoomdl/data/streams.py
@@ -1,3 +1,5 @@
+import re
+
 class Streams:
     def __init__(self, content: dict[str, str]) -> None:
         self._content = content
@@ -9,3 +11,17 @@ class Streams:
     @property
     def speaker(self) -> str:
         return self._content['speakerMp4Url']
+
+    def _sanitize(self, string: str) -> str:
+        return re.sub(r'[^ a-zA-Z0-9_{}()-]', '_', string)
+
+    @property
+    def title(self) -> str:
+        title = self._content['meet']['topic']
+        clips = self._content['totalClips']
+        curr_clip = self._content['currentClip']
+        
+        if clips > 1:
+            return self._sanitize(f'{title} - Recording {curr_clip} of {clips}')
+
+        return self._sanitize(title)
