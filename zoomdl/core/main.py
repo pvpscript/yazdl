607826dec7e42d3ec9af8748c7a37b04cca6a1e2
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2024-05-16 23:56:24 -0300

    chore: adding args to downloader

--- zoomdl/core/main.py
+++ zoomdl/core/main.py
@@ -5,24 +5,27 @@ from zoomdl.handlers.parser import InfoParser
 from zoomdl.handlers.url_handler import UrlHandler
 from zoomdl.handlers.data_handler import DataHandler
 from zoomdl.core.zoom_downloader import ZoomDownloader
+from zoomdl.core.headers import default_headers
 
-def init_downloader(session: requests.Session) -> ZoomDownloader:
+def init_downloader(args: list[str], session: requests.Session) -> ZoomDownloader:
     parser = InfoParser()
 
     url_handler = UrlHandler(session, parser)
     data_handler = DataHandler(session, url_handler)
 
-    return ZoomDownloader(session, data_handler)
+    return ZoomDownloader(args, session, data_handler)
 
-def do_stuff():
-    url = sys.argv[1]
+def start_session(args: list[str]) -> None:
+    url = args[1]
 
     with requests.Session() as session:
-        zoom_downloader = init_downloader(session)
+        session.headers = default_headers
+
+        zoom_downloader = init_downloader(args, session)
 
         zoom_downloader.download_data(url)
 
-def main():
+def main() -> None:
     print("Entering main")
 
-    do_stuff()
+    start_session(sys.argv)
